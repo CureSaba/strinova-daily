886200c803f67edda496c8c1d435242cb91a0604
Keita Furumatsu <a6360152@gmail.com>
2025-07-17 23:44:05 +0900

    ^^

---
+++ main.js
@@ -0,0 +1,21 @@
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client();
+
+const CHANNEL_ID = '1319244621267730432';
+const BOT_ID = '1307317268895498302';
+const COMMAND_NAME = 'daily';
+const TOKEN = process.env.DISCORD_TOKEN;
+
+client.on('ready', async () => {
+    try {
+        console.log('Logged in Successfully');
+        const channel = await client.channels.fetch(CHANNEL_ID);
+        await channel.sendSlash(BOT_ID, COMMAND_NAME);
+        console.log('Daily command sent successfully!');
+    } catch (error) {
+        console.error('Error:', error);
+    }
+    process.exit(0);
+});
+
+client.login(TOKEN);
